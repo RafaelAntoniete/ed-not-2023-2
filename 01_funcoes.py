54b552bd9fdb307121e0d4ace91c5fbec8cb644d
RafaelAntoniete <rafaelantoniete0@gmail.com>
2023-08-11 21:59:54 -0300

    (11/08) Funções

---
+++ 01_funcoes.py
@@ -0,0 +1,12 @@
+
+# Função para calcular IMC de uma pessoa
+def calc_imc(peso, altura):
+    return peso / altura ** 2
+
+# p = 95
+# a = 1.82
+# imc = calc_imc(p, a)
+
+# print(imc)
+
+print (calc_imc(85, 1.72))
